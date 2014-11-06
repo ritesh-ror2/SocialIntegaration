75b9bde13248e1684e6ffa71b6b0e27e51827fe9
GrepRuby <grepruby@GrepRubys-Mac-mini.local>
2014-11-06 11:56:11 +0530

    Initial Commit

---
+++ SocialIntegaration/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SocialIntegaration
+//
+//  Created by GrepRuby on 06/11/14.
+//  Copyright (c) 2014 GrepRuby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SocialIntegaration/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SocialIntegaration
+//
+//  Created by GrepRuby on 06/11/14.
+//  Copyright (c) 2014 GrepRuby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
